b4fd91ebe133a9dbe2b2948206b721802c36728f
phoebekaranja <phoebemurish@gmail.com>
2018-08-13 12:01:50 +0300

    all changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,18 @@
-function getTriangle(){
-var a = parseFloat(prompt("Enter the length side a"));
-var b = parseFloat(prompt("Enter the height side b"));
-var c = parseFloat(prompt("Enter the hypotenuse c"));
-var determine = function(a, b, c){
-if (a === b && b === c && a === c){
-  alert("Equilateral Triange");
-    }
-else if ((a === b && b === a) (a != c && b !== C));
-alert("Isosceles");
-}
- determine();
+function check_getTriangle(){
+var a=parseFloat(prompt("side a"));
+var b=parseFloat(prompt("side b"));
+var c=parseFloat(prompt("side c"));
+
  if(a===b && a===c && b===c){
-alert("Equilateral");
+ alert("Equilateral");
 }
 else if(a===b || a===c || b===c){
-alert(" Its is an Isosceles");
+alert("Its is an Isosceles");
 }
-else if((a+b)<= c || (a+c)<=b || (b+c)<=a) {
+else if((a+b)<= c || (a+c)<=b || (b+c)<=a){
 alert("its not a Triangle");
 }
-else
+else{
 alert("Scalene");
 }
 }
-getTriangle();
-
-
-​
-​
